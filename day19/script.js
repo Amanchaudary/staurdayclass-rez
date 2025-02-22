dc0ac968375f0eee77dc0a25fbd78a5606fbbd5a
amanecdy <amanecdy@gmail.com>
2025-02-22 11:03:31 +0900

    day 19 complete

---
+++ day19/script.js
@@ -0,0 +1,15 @@
+// let a=2
+// let b="2"
+// console.log(a==b)
+
+
+let  markWeight=78;
+let markHight=1.69;
+let BMImark=(markHight/(markHight*markHight))
+let johnWeight=92;
+let johnHight=1.95;
+let BMIjohn=(johnWeight/(johnWeight*johnHight))
+console.log(BMImark)
+console.log(BMIjohn)
+let result=(BMImark>=BMIjohn)
+console.log(result)
