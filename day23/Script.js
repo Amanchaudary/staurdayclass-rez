0af492bc0d332175c4a9fb4c4c379c078b52cb88
amanecdy <amanecdy@gmail.com>
2025-03-30 08:27:55 +0900

    form can be created

---
+++ day23/Script.js
@@ -0,0 +1,10 @@
+//  String makeing mathod
+let a='Hello world'   
+console.log(typeof a);
+
+let b=new String("hello world")
+console.log(typeof b);
+
+// some String method
+let c=b.toUpperCase()
+console.log(c);
