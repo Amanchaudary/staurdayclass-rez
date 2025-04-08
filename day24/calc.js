624fda27a4f5d63fd30249be04862a4557e64164
amanecdy <amanecdy@gmail.com>
2025-04-08 11:20:00 +0900

    error show

--- day24/calc.js
+++ day24/calc.js
@@ -2,3 +2,7 @@ function add(){
     const result=num1+num2
     return result
 }
+const addhtml=document.getElementById("add")
+console.log(addhtml);
+const  result=add(1,2)
+addhtml.textContent=result
