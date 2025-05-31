c3c18c789edbf5d5d14807b0be1f90ae749904bc
amanecdy <amanecdy@gmail.com>
2025-05-31 11:08:13 +0900

    array learn

---
+++ day32/array.js
@@ -0,0 +1,17 @@
+//  Array making style
+// style1 
+const nums=[6,2,7,4,5,1]
+let nums1=nums.sort()
+console.log(nums1);
+// index number with
+console.log(nums1[0]);
+console.log(nums1[2]);
+console.log(nums1[3]);
+// mthode of arry
+
+nums1.push(100)
+nums1.pop()
+nums1.unshift()
+nums.reverse()
+nums.includes(33)
+console.log(nums1);
---
+++ day32/date.js
@@ -0,0 +1,12 @@
+// date with no new key ==> asle string dinxa ra stirng ko function haru use garna sakainxa
+const e=Date()
+console.log(e);
+console.log(typeof e);
+// date with new key
+let d=new Date();  /* new key le jahile ni object banauxa  ra object ko method use garna sakinxa*/
+console.log(d);
+console.log(typeof d);
+let f=d.getMonth()  /*  month chai 0 zero index bata start hunxa  */
+console.log(f);
+h=Date.now()
+console.log(h);
