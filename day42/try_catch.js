133db88ca6e86801db081bbd4baffd728e160faf
チャウダリアマン <146607332+Amanchaudary@users.noreply.github.com>
2025-08-23 08:12:15 +0900

    day42 try and catch learned

---
+++ day42/try_catch.js
@@ -0,0 +1,12 @@
+// const a = 10;
+// const b = 2;
+// const result = a / console.log(result);
+
+// shortcut==>trycatch+enterbutton
+try {
+  const a = 10;
+  const b = 2;
+  const result = a / console.log(result);
+} catch (error) {
+    console.log("error");
+}
