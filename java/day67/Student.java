28f0712cbabe05d487090b73adc87f88119b0601
チャウダリアマン <146607332+Amanchaudary@users.noreply.github.com>
2026-04-18 10:02:35 +0900

    object and pojo and default constructor learned

---
+++ java/day67/Student.java
@@ -0,0 +1,14 @@
+public class Student {
+    int id;
+    String name;
+    // difualt constructor
+    public Student(){
+
+    }
+   
+    // allargsConstructors
+    public Student(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
